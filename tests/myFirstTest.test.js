344d69e496588065322f3296da16f01f95e7b247
rahul kandari <rahulkandari012@gmail.com>
2023-04-01 11:20:01 +0530

    add multiple test in describe

--- tests/myFirstTest.test.js
+++ tests/myFirstTest.test.js
@@ -1,5 +1,13 @@
-test("1 plus 1 equal to 2", () => {
-  let a = 1;
-  let b = 2;
-  expect(a + b).toBe(3);
+describe("number operations", () => {
+  test("1 plus 1 equal to 2", () => {
+    let a = 1;
+    let b = 2;
+    expect(a + b).toBe(3);
+  });
+
+  test("2 plus 3 not equal to 3", () => {
+    let a = 2;
+    let b = 3;
+    expect(a + b).not.toBe(3);
+  });
 });
