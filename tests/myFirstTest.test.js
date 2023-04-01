b24cc2870f8a209ef53ac48defac8a593c3dab9a
rahul kandari <rahulkandari012@gmail.com>
2023-04-01 18:18:22 +0530

    basic fxn testing

--- tests/myFirstTest.test.js
+++ tests/myFirstTest.test.js
@@ -1,58 +1,7 @@
-describe("testing reference equality", () => {
-  const user = {
-    name: "rahul",
-  };
-  user.age = 45;
+const { sum } = require("../utils/practice");
 
-  test("should return a user with age 45", () => {
-    expect(user).toEqual({
-      // for reference type use toEqual instead of toBe
-      name: "rahul",
-      age: 45,
-    });
-  });
-
-  test("should return a user with age and name key", () => {
-    expect(user).toEqual(
-      expect.objectContaining({
-        name: expect.any(String),
-        age: expect.any(Number),
-      })
-    );
-  });
-
-  //   array equality
-
-  test("Array enuality", () => {
-    const users = ["rahul", "sara", "ab"];
-
-    users.push("jacob");
-    expect(users).toEqual(["rahul", "sara", "ab", "jacob"]);
-    expect(users).toEqual(expect.arrayContaining([expect.any(String)]));
-
-    const userObject = [
-      {
-        name: "clement",
-        age: 12,
-      },
-      {
-        name: "rk",
-        age: 19,
-      },
-      ,
-      {
-        name: "raja",
-        age: 35,
-      },
-    ];
-
-    expect(userObject).toEqual(
-      expect.arrayContaining([
-        expect.objectContaining({
-          name: expect.any(String),
-          age: expect.any(Number),
-        }),
-      ])
-    );
+describe("testing imported fxn", () => {
+  test("sum function", () => {
+    expect(sum(5, 3)).toBe(8);
   });
 });
