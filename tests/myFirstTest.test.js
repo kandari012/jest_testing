9b0398b326a3462cf5a46bf8f9a8b4a2713fc9cb
rahul kandari <rahulkandari012@gmail.com>
2023-04-02 11:22:45 +0530

    more fxn

--- tests/myFirstTest.test.js
+++ tests/myFirstTest.test.js
@@ -1,7 +1,39 @@
-const { sum } = require("../utils/practice");
+const { sum, deleteUser } = require("../utils/practice");
 
 describe("testing imported fxn", () => {
   test("sum function", () => {
     expect(sum(5, 3)).toBe(8);
+  });
+
+  test("delete by ID function", () => {
+    let user = [
+      {
+        name: "rk1",
+        age: 23,
+        id: 1,
+      },
+      {
+        name: "rk2",
+        age: 28,
+        id: 2,
+      },
+      {
+        name: "rk3",
+        age: 26,
+        id: 3,
+      },
+    ];
+    expect(deleteUser(user, 3)).toEqual([
+      {
+        name: "rk1",
+        age: 23,
+        id: 1,
+      },
+      {
+        name: "rk2",
+        age: 28,
+        id: 2,
+      },
+    ]);
   });
 });
--- utils/practice.js
+++ utils/practice.js
@@ -1,3 +1,9 @@
 exports.sum = (a, b) => {
   return a + b;
 };
+
+exports.deleteUser = (arr, id) => {
+  return arr.filter((user) => {
+    return user.id !== id;
+  });
+};
