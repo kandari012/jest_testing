be791df72be847e863ebf9b593fbd8d70cfe6788
rahul kandari <rahulkandari012@gmail.com>
2023-04-02 11:42:00 +0530

    common fxn

--- tests/myFirstTest.test.js
+++ tests/myFirstTest.test.js
@@ -1,5 +1,21 @@
 const { sum, deleteUser } = require("../utils/practice");
 
+let userdata = [];
+beforeAll(() => {
+  console.log("run before all test");
+  userdata = ["clement", "saerah"];
+});
+beforeEach(() => {
+  console.log("running  before each test");
+});
+afterEach(() => {
+  console.log("running  after each test");
+});
+afterAll(() => {
+  console.log("run after all test");
+  userdata = [];
+});
+
 describe("testing imported fxn", () => {
   test("sum function", () => {
     expect(sum(5, 3)).toBe(8);
